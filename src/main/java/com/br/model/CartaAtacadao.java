0f2bf23625e152c73eed257a5e185580314a9a5b
danieljt84 <danieljt84@gmail.com>
2022-02-09 17:35:59 -0300

    alteração

---
+++ src/main/java/com/br/model/CartaAtacadao.java
@@ -0,0 +1,17 @@
+package com.br.model;
+
+public class CartaAtacadao extends ModeloCarta{
+	
+	private String estado;
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+
+}
