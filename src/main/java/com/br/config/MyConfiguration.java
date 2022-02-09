9f5ca7513e12f1c9e1fb3955c6a66bb3fa964ba2
danieljt84 <danieljt84@gmail.com>
2022-02-09 11:48:29 -0300

    alteracao do cors 2

--- src/main/java/com/br/config/MyConfiguration.java
+++ src/main/java/com/br/config/MyConfiguration.java
@@ -2,15 +2,16 @@ package com.br.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MyConfiguration implements WebMvcConfigurer  {
 	
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("https://modelo-carta-angular.herokuapp.com")
-                .allowedMethods("*");
+        registry.addMapping("/**");
     }
 
 	
